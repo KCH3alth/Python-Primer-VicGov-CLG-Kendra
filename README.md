be89d4cef122d18a50e74e1ffc45a5e6ed6fad18
Kendra C <kendra.cisternas@health.vic.gov.au>
2024-02-22 17:59:46 +1100

    Uploading assignment 4

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 - [x] Assignment 1 - *'Code Like a Girl'*
 - [x] Assignment 2 - ***Annoying*** *text*
 - [x] Assignment 3 - *Savings calculator*
-- [ ] Assignment 4 -
+- [x] Assignment 4 - Palindrome
 - [ ] Assignment 5 -
 - [ ] Assignment 6 -
 - [ ] Assignment 7 -
---
+++ python-lesson-04/assignment_4_palindrome.py
@@ -0,0 +1,24 @@
+# Write some code that will take an input word from a user and check if this word is a palindrome.
+word = input("Please submit a word to check if its a palindrome: ")
+
+letters = list(word.lower())
+## I added the .lower() because during testing I found the result would always say false if sentence case had been used on the input word.
+letters.reverse()
+reversed_word = "".join(letters)
+palindrome_check = (word.lower() == reversed_word)
+
+print(f"Is {word} a palindrome? {palindrome_check}.\n")
+
+## Alternate sentences to test 'if/else' and make a better sounding answer. 
+## If the users input was not a palindrome it will show them what their input looks like when reversed.
+if palindrome_check == True:
+    is_isnt = 'is'
+else:
+    is_isnt = 'is not'
+
+if is_isnt == 'is not':
+    not_palindrome = (f" {word} backwards is {reversed_word}")
+else:
+    not_palindrome = ''
+
+print(f"Your suggestion '{word}', {is_isnt} a palindrome.{not_palindrome}\n")
