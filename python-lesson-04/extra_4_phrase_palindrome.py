347d3d93c1a3cc415fa126efc299a43769f46da9
Kendra C <kendra.cisternas@health.vic.gov.au>
2024-02-23 18:38:12 +1100

    updated draft

--- python-lesson-04/extra_4_phrase_palindrome.py
+++ python-lesson-04/extra_4_phrase_palindrome.py
@@ -1,3 +1,4 @@
+# IN DRAFT
 # Version to check if a phrase is a palindrome
 phrase = input("Please submit a phrase to check if it is a palindrome: ")
 
@@ -8,10 +9,8 @@ phrase_no_space = "".join(phrase_letters)
 phrase_letters.reverse()
 print(phrase_letters)
 reversed_phrase = "".join(phrase_letters)
-print(reversed_phrase)
 
 palindrome_check = (phrase_no_space == reversed_phrase.lower())
-print(palindrome_check)
 if palindrome_check == True:
     is_isnt = 'is'
     not_palindrome = ''
